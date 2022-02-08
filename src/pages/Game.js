71dea3f4276f6f5b46ebb64d2e8d7c831084a3f2
Svetoslav Petrov <s.petrov@proxiad.com>
2022-02-08 22:02:50 +0200

    Now Game component is sending POST request and printing the response data

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,9 +1,30 @@
 
 const Game = () => {
+
+    // send POST request using useSwr
+    const fetcher = () => {
+        const endpointURL = "http://localhost:8080/games";
+        fetch(endpointURL, {
+            method: 'POST'
+        }).then(
+            data => data.json()
+        ).then(
+            data => console.log(data)
+        )
+    };
+
+    fetcher();
+
+
     return (
-        <div>
-            The Game started
-        </div>);
+        <main>
+            <h2> The Game started </h2>
+
+            <p> Word element should be placed here </p>
+
+            <p> Lives - 5 </p>
+
+        </main>);
 };
 
 export default Game;
